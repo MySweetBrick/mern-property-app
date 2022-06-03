6611a8a2c2c9942328c457f7c46c4e0bf17509cf
Terumi McIntyre <terumi.mc@gmail.com>
2022-06-03 23:02:53 +1200

    added query and sorting

--- server/controllers/propertyController.js
+++ server/controllers/propertyController.js
@@ -6,20 +6,49 @@ const propertySchema = require('../models/propertyModel');
 // });
 
 getProperties = async (req, res) => {
-    await propertySchema.find({}, (err, property) => {
-      if (err) {
-        return res.status(400).json({ success: false, error: err });
-      }
-      if (!propertySchema.length) {
-        return res.status(404).json({ success: false, error: `Property not found` });
-      }
+
+  let query;
+
+  const reqQuery = { ...req.query };
+
+  const removeFields = ["sort"];
+
+  removeFields.forEach(val => delete reqQuery[val]);
+
+  let queryStr = JSON.stringify(reqQuery);
+ 
+  queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, (match) => `$${match}`);
+
+  query = propertySchema.find(JSON.parse(queryStr));
+
+  if (req.query.sort) {
+    const sortByArr = req.query.sort.split(',');
+    const sortByStr = sortByArr.join(' ');
+
+    query = query.sort(sortByStr);
+  }
   
-      return res.status(200).json({ success: true, data: property });
-    })
-      .clone()
-      .catch(err => console.log(err));
-  };
+  const property = await query;
+
+  res.status(200).json({
+    success: true,
+    data: property
+  });
+
+  // await propertySchema.find(JSON.parse(queryStr), (err, property) => {
+  //   if (err) {
+  //     return res.status(400).json({ success: false, error: err });
+  //   }
+  //   if (!propertySchema.length) {
+  //     return res.status(404).json({ success: false, error: `Property not found` });
+  //   }
+
+  //   return res.status(200).json({ success: true, data: property });
+  // })
+  //   .clone()
+  //   .catch(err => console.log(err));
+};
 
 module.exports = {
-    getProperties
+  getProperties
 };
